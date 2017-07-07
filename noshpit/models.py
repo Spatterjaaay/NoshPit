03b4b77e7c3af557b98dd503347b2a3089f2b206
Spatterjaaay <aja.pursova@gmail.com>
2017-07-06 21:28:17 -0700

    created models for Location, Photo, Pit, User, and Vote

--- noshpit/models.py
+++ noshpit/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    place_id = models.CharField(max_length=300)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    location = models.ForeignKey(Location)
+    url = models.CharField(max_length=300)
+
+class Pit(models.Model):
+    token = models.CharField(max_length=5)
+
+class User(models.Model):
+    pass
+
+class Vote(models.Model):
+    pit = models.ForeignKey(Pit)
+    user = models.ForeignKey(User)
+    location = models.ForeignKey(Location)
