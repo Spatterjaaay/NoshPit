fdbf4116ddb360ec7e6ffba25a6b9f3b0ca2c0aa
Spatterjaaay <aja.pursova@gmail.com>
2017-07-06 21:45:03 -0700

    registered models with admin

--- noshpit/admin.py
+++ noshpit/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Location)
+admin.site.register(Photo)
+admin.site.register(Pit)
+admin.site.register(User)
+admin.site.register(Vote)
