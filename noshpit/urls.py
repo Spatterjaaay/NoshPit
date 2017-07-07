1cc28245d679dbb634a3a4ba426421713467cee7
Spatterjaaay <aja.pursova@gmail.com>
2017-07-07 11:02:46 -0700

    added tempaltes, views and routes for starting a pit and joining a pit

--- noshpit/urls.py
+++ noshpit/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^start$', views.start_a_pit, name='start'),
+    url(r'^join$', views.join_a_pit, name='join'),
 ]
--- noshpit/views.py
+++ noshpit/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'noshpit/home.html', {})
+
+def start_a_pit(request):
+    return render(request, 'noshpit/start_a_pit.html', {})
+    pass
+
+def join_a_pit(request):
+    return render(request, 'noshpit/join_a_pit.html', {})
+    pass
