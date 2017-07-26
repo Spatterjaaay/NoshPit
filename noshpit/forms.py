a75a9bc0b1a08219a50f95460c473ff2eb432ebe
Spatterjaaay <aja.pursova@gmail.com>
2017-07-26 14:09:27 -0700

    added styling to all forms in the application

--- noshpit/forms.py
+++ noshpit/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 class PitForm(forms.Form):
-    distance = forms.IntegerField(label='Distance', min_value=500, max_value=5000, widget=forms.TextInput(attrs={'placeholder': 'Enter Distance in m'}))
+    distance = forms.IntegerField(label='Distance', min_value=500, max_value=5000, widget=forms.TextInput(attrs={'placeholder': 'Enter Distance in m', 'class': 'form-control col-sm-5'}))
 
 class JoinForm(forms.Form):
-    token = forms.CharField(label='Token', min_length=5, max_length=5, widget=forms.TextInput(attrs={'placeholder': 'Enter Token'}))
+    token = forms.CharField(label='Token', min_length=5, max_length=5, widget=forms.TextInput(attrs={'placeholder': 'Enter Token', 'class': 'form-control col-sm-3'}))
 
 class EmailForm(forms.Form):
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'placeholder': 'Enter Email', 'class': 'form-control col-sm-5','id': 'inputEmail3'}))
